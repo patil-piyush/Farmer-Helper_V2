2c0e82f3f0c66e65be1c7b80d164599c83b39d2e
Piyush <piyushpatil0031@gmail.com>
2026-05-12 13:46:55 +0530

    region hardcoded

--- backend/config/dynamodb.js
+++ backend/config/dynamodb.js
@@ -2,7 +2,7 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
 const client = new DynamoDBClient({
-    region: process.env.AWS_REGION,
+    region: "eu-north-1",
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
