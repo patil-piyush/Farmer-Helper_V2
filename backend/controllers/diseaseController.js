e6c56b1eb8fd1be9d1e24359badaf9a961875d77
Piyush Patil <piyushpatil0031@gmail.com>
2026-08-25 15:46:22 +0530

    feat: add support for generating signed URLs for private S3 buckets

--- backend/controllers/diseaseController.js
+++ backend/controllers/diseaseController.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
 const FormData = require("form-data");
+const { GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const s3 = require("../config/s3");
 require("dotenv").config();
 
 const mlServiceUrl = process.env.ML_SERVICE_URL || "http://ml_service:5001";
@@ -14,8 +17,22 @@ const detectDisease = async (req, res) => {
         const formData = new FormData();
 
         // send S3 URL instead of file
-        formData.append("image_url", req.file.location);
+        // if bucket is public 
+        // formData.append("image_url", req.file.location);
 
+        // if bucket is private, generate a signed URL
+        const command = new GetObjectCommand({
+            Bucket: "farmer-helper-storage-devops",  // Replace with your bucket name
+            Key: req.file.key
+        });
+
+        const signedUrl = await getSignedUrl(s3, command, {
+            expiresIn: 300
+        });
+
+        formData.append("image_url", signedUrl);
+        
+        
         const response = await axios.post(
             `${mlServiceUrl}/predict/disease`,
             formData,
